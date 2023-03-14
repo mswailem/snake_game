cb7c7f62f233a4eaf225b1248e5668b28a6bb56f
mswailem <mswailem@vt.edu>
2023-03-13 20:13:20 -0400

    fixed collisions with food, but collision system still needs to be made better

--- collision_handler.cpp
+++ collision_handler.cpp
@@ -1,4 +1,5 @@
 #include "collision_handler.h"
+#include <cmath>
 
 
 CollisionHandler::~CollisionHandler() {
@@ -15,8 +16,10 @@ void CollisionHandler::detect_collisions(Player* player, Food* food, sf::VideoMo
 		player->velocity.y = 0;
 	}
 	float dx = player->position[0].x - food->position.x;
+	int sign_dx = ceil(dx/std::abs(dx));
 	float dy = player->position[0].y - food->position.y;
-	if (std::abs(dx) <= (player->width/2+food->width/2) && std::abs(dy) <= (player->height/2+food->height/2)) {
+	int sign_dy = ceil(dy/std::abs(dy));
+	if (std::abs(dx) <= (player->width/2+food->width/2) - sign_dx*(player->width * player->velocity.x) && std::abs(dy) <= (player->height/2+food->height/2) - sign_dy*(player->width * player->velocity.y)) {
 		food->reposition();
 		player->shape.push_back(sf::RectangleShape());
 		player->position.push_back(player->position[player->size-1]+sf::Vector2f(player->right_edge, 0));
